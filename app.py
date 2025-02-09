e4cb01159bc3816a9ccc306457736106ff19c214
Temirlan <temirlan.asamidinov@gmail.com>
2025-02-08 20:17:24 -0600

    I hope

--- app.py
+++ app.py
@@ -1,28 +1,21 @@
 from flask import Flask, render_template, request, redirect, session
-from flask_session import Session
-from cs50 import SQL
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-db = SQL("sqlite:///my_db.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///simy_dbte.db' 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+db = SQLAlchemy(app)
 
-@app.route("tasamidinov.github.io/")
+@app.route("/tasamidinov.github.io/")
 def index():
     return render_template("books.html", books=db.execute("SELECT * FROM books"))
 
-@app.route("tasamidinov.github.io/cart", methods = ["POST", "GET"])
+@app.route("/tasamidinov.github.io/cart", methods = ["POST", "GET"])
 def cart():
-    if "cart" not in session:
-        session["cart"] = []
-
     if request.method == "POST":
         book_id = request.form.get("id")
-        if book_id:
-            session["cart"].append(book_id)
         return redirect("/cart")
 
     books = db.execute("SELECT * FROM books WHERE id IN (?)", session["cart"])
