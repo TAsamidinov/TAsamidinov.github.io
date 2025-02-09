acdc4574925e7b899c1a676819ca5320644e66cb
Temirlan <temirlan.asamidinov@gmail.com>
2025-02-08 18:21:43 -0600

    commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect, session
+from flask_session import Session
+from cs50 import SQL
+
+app = Flask(__name__)
+
+db = SQL("sqlite:///my_db.db")
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/")
+def index():
+    return render_template("books.html", books=db.execute("SELECT * FROM books"))
+
+@app.route("/cart", methods = ["POST", "GET"])
+def cart():
+    if "cart" not in session:
+        session["cart"] = []
+
+    if request.method == "POST":
+        book_id = request.form.get("id")
+        if book_id:
+            session["cart"].append(book_id)
+        return redirect("/cart")
+
+    books = db.execute("SELECT * FROM books WHERE id IN (?)", session["cart"])
+    return render_template("cart.html", books = books)
